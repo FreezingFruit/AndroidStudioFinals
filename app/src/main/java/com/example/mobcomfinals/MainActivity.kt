f9ead2be18faa814211179712fd953e83b62462a
JHale <89829445+burpey@users.noreply.github.com>
2023-11-13 13:40:20 +0800

    test

--- app/src/main/java/com/example/mobcomfinals/MainActivity.kt
+++ app/src/main/java/com/example/mobcomfinals/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mobcomfinals
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    companion object {
+        fun launch(activity : Activity) = activity.startActivity(Intent(activity, MainActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/example/mobcomfinals/ui/SignUp.kt
@@ -0,0 +1,52 @@
+package com.example.mobcomfinals.ui
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobcomfinals.R
+import com.example.mobcomfinals.databinding.ActivitySignUpBinding
+
+class SignUp : AppCompatActivity() {
+    private lateinit var binding : ActivitySignUpBinding
+    private lateinit var viewModel : AuthenticationViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = AuthenticationViewModel()
+        viewModel.getStates().observe(this@SignUp) {
+            handleState(it)
+        }
+
+        binding.btnSignup.setOnClickListener {
+            viewModel.signUp(
+                binding.tieEmail.text.toString(),
+                binding.tiePassword.text.toString()
+            )
+        }
+
+    }
+
+    private fun handleState(state : AuthenticationStates) {
+        when(state) {
+            is AuthenticationStates.SignedUp -> viewModel.createUserRecord(
+                binding.tieEmail.text.toString(),
+                binding.tieName.text.toString(), "0")
+
+
+            is AuthenticationStates.ProfileUpdated -> {
+                LoginActivity.launch(this@SignUp)
+                finish()
+            }
+            else -> {}
+        }
+    }
+
+
+    companion object {
+        fun launch(activity: Activity) = activity.startActivity(Intent(activity, SignUp::class.java))
+    }
+}
